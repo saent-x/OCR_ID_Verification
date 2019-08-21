8354ad90527f97ee741ef2b3babfccb894a41eec
Torsxperience <Torsxperience@outlook.com>
2019-08-21 13:49:22 +0100

    corrected argument name

--- tr.py
+++ tr.py
@@ -2,12 +2,12 @@ from passporteye.mrz.image import MRZPipeline
 import argparse
 
 ap = argparse.ArgumentParser()
-ap.add_argument("-d", "--doc", type=str, help="image path")
+ap.add_argument("-i", "--image", type=str, help="image path")
 
 args = vars(ap.parse_args())
 
 # so your preprocessing code would enter here, the image file is in args["doc"],
 # then pass the result into MRZPipeline(*,extra_cmdline_params="--oem 0")
 
-mrz = MRZPipeline(args["doc"],extra_cmdline_params="--oem 0")
+mrz = MRZPipeline(args["image"],extra_cmdline_params="--oem 0")
 print(mrz.result)
